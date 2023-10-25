fd6c2651f385657ed1128ef3dab8fe569a564323
paulonc <paulo.cavalcanti@ccc.ufcg.edu.br>
2023-10-25 19:06:33 -0300

    Add image to 'historyPage' and make page layout improvements

--- frontend/src/page/HistoriaPage.js
+++ frontend/src/page/HistoriaPage.js
@@ -1,24 +1,41 @@
 import React from "react";
-import { Typography, Box, Paper } from "@mui/material";
 import { useLocation } from "react-router-dom";
+import {
+  Container,
+  Typography,
+  Paper,
+  CardMedia,
+  CardContent,
+} from "@mui/material";
 import BackgroundLayout from "../components/Layout/BackgroundLayout";
 
 const HistoriaPage = () => {
   const location = useLocation();
   const historyData = location.state.historyData;
-  const { title, history } = historyData;
+  const { title, history, image } = historyData;
 
   const headerStyle = {
     fontFamily: "'Dancing Script', cursive",
-    fontWeight: 700,
+    fontWeight: 800,
     fontSize: "5rem",
     marginTop: "20px",
+    textAlign: "center",
   };
 
-  const titleStyle = {
-    fontWeight: 800,
-    textAlign: "center",
-    textTransform: "uppercase",
+  const containerStyle = {
+    overflowY: "scroll",
+    maxHeight: "100vh",
+  };
+
+  const paperStyle = {
+    padding: "20px",
+    marginTop: "20px",
+    height: "auto",
+  };
+
+  const storyTextStyle = {
+    fontSize: "20px",
+    fontWeight: 600,
   };
 
   const renderParagraphs = (text) => {
@@ -29,27 +46,29 @@ const HistoriaPage = () => {
 
   return (
     <BackgroundLayout>
-      <Box
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        minHeight="100vh"
-        maxWidth="md"
-      >
+      <Container maxWidth="lg" style={containerStyle}>
         <Typography variant="h3" style={headerStyle} gutterBottom>
-          Sua história
+          {title}
         </Typography>
-        <Paper elevation={3} style={{ padding: "20px"}}>
-          <Typography variant="h6" style={titleStyle} gutterBottom>
-            {title}
-          </Typography>
-          <div style={{ maxHeight: "400px", overflowY: "auto" }}>
-            <Typography variant="body1" component="div">
+        <Paper elevation={3} style={paperStyle}>
+          <CardMedia
+            component="img"
+            alt="Story Image"
+            style={{
+              height: "512px",
+              width: "auto",
+              margin: "0 auto",
+              display: "block",
+            }}
+            image={image}
+          />
+          <CardContent>
+            <Typography variant="body1" style={storyTextStyle}>
               {renderParagraphs(history)}
             </Typography>
-          </div>
+          </CardContent>
         </Paper>
-      </Box>
+      </Container>
     </BackgroundLayout>
   );
 };
